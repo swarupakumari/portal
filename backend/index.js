129268d41c0a25234413fcd77649124c9558289c
swarupa kumari <swarupakumari126@gmail.com>
2025-07-16 08:22:15 +0530

    made jobmodel

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
+// import userRoute from "./routes/user.route.js";
+// import companyRoute from "./routes/company.route.js";
+// import jobRoute from "./routes/job.route.js";
+// import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
@@ -27,10 +27,10 @@ const PORT = process.env.PORT || 3000;
 
 
 // api's
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
+// app.use("/api/v1/user", userRoute);
+// app.use("/api/v1/company", companyRoute);
+// app.use("/api/v1/job", jobRoute);
+// app.use("/api/v1/application", applicationRoute);
 
 
 
