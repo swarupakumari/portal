d98fa49b81003ed619a1cb3ac1f9e8563187e256
swarupa kumari <swarupakumari126@gmail.com>
2025-07-16 08:15:33 +0530

    setting up database

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
-
-
+dotenv.config({});
 
 const app = express();
-
 
 // middleware
 app.use(express.json());
@@ -19,19 +23,18 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT =  3000;
+const PORT = process.env.PORT || 3000;
 
 
-// api's       
-// app.use("/api/v1/user", userRoute);
-// app.use("/api/v1/company", companyRoute);
-// app.use("/api/v1/job", jobRoute);
-// app.use("/api/v1/application", applicationRoute);
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 
 
 app.listen(PORT,()=>{
+    connectDB();
     console.log(`Server running at port ${PORT}`);
-
-    
 })
